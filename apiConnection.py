f7bad5007b205985958786bb34e54fe89ae3a09a
Michał <teranowa1995@gmail.com>
2024-05-17 21:27:42 +0200

    approach on api connection v2

--- apiConnection.py
+++ apiConnection.py
@@ -1,28 +1,14 @@
 import sys
+import requests
 sys.path.append("../")
 
-from igdb.wrapper import IGDBWrapper
 from secure import passwords
-wrapper = IGDBWrapper("GameComparison", passwords.APIKEY)
 
-'''With a wrapper instance already created'''
-# JSON API request
-byte_array = wrapper.api_request(
-            'games',
-            'fields id, name; offset 0; where platforms=48;'
-          )
-# parse into JSON however you like...
+myHeader={
+    "client_id":passwords.CLIENTID,
+    "client_secret" : passwords.APIKEY,
+    "grant_type":"client_credentials"
+}
 
-# Protobuf API request
-from igdb.igdbapi_pb2 import GameResult
-byte_array = wrapper.api_request(
-            'games.pb', # Note the '.pb' suffix at the endpoint
-            'fields id, name; offset 0; where platforms=48;'
-          )
-games_message = GameResult()
-games_message.ParseFromString(byte_array) # Fills the protobuf message object with the response
-games = games_message.games
-
-print(games)
-
-
+response = requests.post("https://id.twitch.tv/oauth2/", headers= myHeader)
+print(response.text)
